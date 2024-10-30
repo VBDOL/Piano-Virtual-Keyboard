8fa15c388855c788d6c1eb8514583832910d4c9e
VBDOL. DEV <155402702+VBDOL@users.noreply.github.com>
2024-10-30 07:00:06 -0300

    Add files via upload

---
+++ src/scripts/engine.js
@@ -0,0 +1,50 @@
+/*
+DESENVOLVEDOR: VBDOL.DEV
+GITHUB: https://github.com/VBDOL
+LINKEDIN: https://www.linkedin.com/in/victor-b-o-leme-dev
+Alura: https://cursos.alura.com.br/user/vbdol-dev
+DIO: https://www.dio.me/users/vbdol_dev
+
+DATA: 26/10/2024
+*/
+
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
+
+let mapedKeys = [];
+let audio = new Audio("src/tunes/a.wav");
+
+const playTune = (key) => {
+  audio.src = `src/tunes/${key}.wav`;
+  audio.play();
+
+  const clickedKey = document.querySelector(`[data-key="${key}"]`);
+  clickedKey.classList.add("active");
+  setTimeout(() => {
+    clickedKey.classList.remove("active");
+  }, 150);
+};
+
+pianoKeys.forEach((key) => {
+  key.addEventListener("click", () => playTune(key.dataset.key));
+  mapedKeys.push(key.dataset.key);
+});
+
+document.addEventListener("keydown", (e) => {
+  if (mapedKeys.includes(e.key)) {
+    playTune(e.key);
+  }
+});
+
+const handleVolume = (e) => {
+  audio.volume = e.target.value;
+};
+
+const showHideKeys = () => {
+  pianoKeys.forEach((key) => key.classList.toggle("hide"));
+};
+
+volumeSlider.addEventListener("input", handleVolume);
+
+keysCheck.addEventListener("click", showHideKeys);
